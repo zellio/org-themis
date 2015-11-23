6f18101bf92c30077945007b852bb18ae60d4eef
Zachary Elliott <contact@zell.io>
2015-11-22 22:00:29 -0500

    Adding project add/remove/set functions

--- org-themis.el
+++ org-themis.el
@@ -55,10 +55,10 @@
 (defun org-themis--load-project-alist ()
   ""
   (if (file-exists-p org-themis-project-alist-file)
-	  (with-temp-buffer
-		(insert-file-contents org-themis-project-alist-file)
-		(read (current-buffer)))
-	'()))
+      (with-temp-buffer
+        (insert-file-contents org-themis-project-alist-file)
+        (read (current-buffer)))
+    '()))
 
 (defun org-themis--save-project-alist ()
   ""
@@ -74,5 +74,61 @@
 (defun org-themis--list-projects ()
   ""
   (mapcar #'car org-themis-project-alist))
+
+(defun org-themis--add-project (name root scratch journal)
+  ""
+  (push
+   (cons name
+         (list
+          (cons 'name (symbol-name name))
+          (cons 'root root)
+          (cons 'scratch scratch)
+          (cons 'journal journal)))
+   org-themis-project-alist))
+
+(defun org-themis-add-project-interactive (name)
+  ""
+  (interactive "SProject Name: ")
+  (if (null (assoc name org-themis-project-alist))
+      (org-themis--add-project
+       name
+       (expand-file-name (symbol-name name) org-themis-project-root)
+       "scratch.org"
+       "journal.org")
+    (error "Name %s already in use" name)))
+
+(defun org-themis--remove-project (name)
+  ""
+  (setq
+   org-themis-project-alist
+   (remove-if
+    (lambda (x) (eql name (car x)))
+    org-themis-project-alist)))
+
+(defun org-themis-remove-project-interactive (name)
+  ""
+  (interactive "SProject Name: ")
+  (org-themis--remove-project name))
+
+(defvar org-themis-project 'nil
+  "")
+
+(defvar org-themis-project-data 'nil
+  "")
+
+(defun org-themis-set-project (name data)
+  ""
+  (setq org-themis-project name)
+  (setq org-themis-project-data data))
+
+(defun org-themis-set-project-interactive (name)
+  ""
+  (interactive "SProject Name: ")
+  (let ((data (assoc name org-themis-project-alist)))
+    (if data
+        (progn
+          (org-themis-set-project name data)
+          (messsage "Project set to %s" name))
+      (error "No project named %s found" name))))
 
 ;;; org-themis.el ends here
